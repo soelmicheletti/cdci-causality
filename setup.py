e261df319ae6c5fa3d7876c8aa5813c87ba98cfa
Soel Micheletti <msoel@ethz.ch>
2022-05-11 17:45:36 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="cdci-causality",
     packages=find_packages(exclude=[]),
-    version="0.0.4",
+    version="0.0.2",
     license="MIT",
     description="CDCI - Causality",
     long_description="CDCI - Causality",
